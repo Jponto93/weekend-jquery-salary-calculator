bdb1291066484a7f4e08961ff71b5be4043e8613
Jordan Ponto <jponto93@gmail.com>
2021-10-01 16:32:39 -0500

    submit employee function now uses helper function to append list of employees to dom

--- client.js
+++ client.js
@@ -4,33 +4,82 @@ $(readyNow);
 
 let employeeList = [];
 
-function readyNow () {
+function readyNow() {
     console.log('JQ');
 
     // CLICK EVENTS
     $('#submitEmployee').on('click', submitEmployee)
-    
+
 } // end readyNow
 
-function submitEmployee () {
-    console.log('in submit employee');
+function submitEmployee() {
+    console.log('in submit employee'); // test log
 
-   const firstName = $('#firstIn').val(); //getting First Name input
-   const lastName = $('#lastIn').val();
-   const id = $('#idIn').val();
-   const title = $('#titleIn').val();
-   const annualSalary = $('#salaryIn').val();
-   //test logs
-  let newEmployee = {
-      firstName: firstName,
-      lastName: lastName,
-      id: id,
-      title: title,
-      annualSalary: annualSalary
-  }
- 
-  employeeList.push(newEmployee);
-  console.log(employeeList);
+    const firstName = $('#firstIn').val();      //getting first name input
+    const lastName = $('#lastIn').val();        //getting last name input
+    const id = $('#idIn').val();                //getting id input
+    const title = $('#titleIn').val();          //getting title input
+    const annualSalary = $('#salaryIn').val();  //getting annual salary input
 
-    
+
+    if (firstName && lastName && id && title && annualSalary) { // if all inputs are true
+
+        //Create new employee object
+        let newEmployee = {
+            firstName: firstName,
+            lastName: lastName,
+            id: id,
+            title: title,
+            annualSalary: annualSalary
+        } // end if
+
+        //push new employee to list
+        employeeList.push(newEmployee);
+        console.log(employeeList);
+
+        //append to dom
+        render();
+
+        //clear inputs
+        $('#firstIn').val('');
+        $('#lastIn').val('');
+        $('#idIn').val('');
+        $('#titleIn').val('');
+        $('#salaryIn').val('');
+
+    } else {   // inputs empty
+        console.log('Input missing!');
+
+        alert('Input missing!');
+
+    }
+
 } // submitEmployee
+
+function render() {
+    console.log('in render');
+    //empty rows
+    $('#employeeRows').empty();
+    // loop to append all employees from list
+    for (let employee of employeeList) {
+
+
+        // created row to append using employee object
+        let row = $(`
+    <tr>
+        <td>${employee.firstName}</td>
+        <td>${employee.lastName}</td>
+        <td>${employee.id}</td>
+        <td>${employee.title}</td>
+        <td>${employee.annualSalary}</td>
+        <td><button id="deleteButton">Delete</button></td>
+    </tr>
+    `);
+    
+   
+
+        $('#employeeRows').append(row)
+
+    } // end for loop
+
+} // end render
