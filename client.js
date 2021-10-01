d4f625afe396d182bb728a84421c1783b3e73c97
Jordan Ponto <jponto93@gmail.com>
2021-10-01 15:52:18 -0500

    submit employee function added, accepts inputs creates new employee object and pushes into employee list array

--- client.js
+++ client.js
@@ -2,7 +2,35 @@ console.log('JS');
 
 $(readyNow);
 
+let employeeList = [];
+
 function readyNow () {
     console.log('JQ');
+
+    // CLICK EVENTS
+    $('#submitEmployee').on('click', submitEmployee)
     
 } // end readyNow
+
+function submitEmployee () {
+    console.log('in submit employee');
+
+   const firstName = $('#firstIn').val(); //getting First Name input
+   const lastName = $('#lastIn').val();
+   const id = $('#idIn').val();
+   const title = $('#titleIn').val();
+   const annualSalary = $('#salaryIn').val();
+   //test logs
+  let newEmployee = {
+      firstName: firstName,
+      lastName: lastName,
+      id: id,
+      title: title,
+      annualSalary: annualSalary
+  }
+ 
+  employeeList.push(newEmployee);
+  console.log(employeeList);
+
+    
+} // submitEmployee
